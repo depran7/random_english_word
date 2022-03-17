2710b9598a7a8eecf437d81a68ecea3e9b5a8141
ade pranaya 153040050 <ade.pranaya@mail.unpas.ac.id>
2022-03-17 14:03:12 +0700

    halaman baru dengan navigator

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,63 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final randomWord = WordPair.random();
+    return const MaterialApp(title: 'Selamat Datang', home: RandomWord());
+  }
+}
 
-    return MaterialApp(
-      title: 'Selamat Datang',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Aplikasi Flutter Pertama'),
+class RandomWord extends StatefulWidget {
+  const RandomWord({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordState createState() => _RandomWordState();
+}
+
+class _RandomWordState extends State<RandomWord> {
+  var savedWords = [];
+  String randomWord = WordPair.random().asString;
+
+  void _changeWord() {
+    setState(() {
+      randomWord = WordPair.random().asString;
+    });
+  }
+
+  void _saveWord() {
+    savedWords.add(randomWord);
+    // halaman baru
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(
+            title: const Text('Saved Words'),
+          ),
+          body: Center(
+            child: Text(savedWords.join(" - ")),
+          ));
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Aplikasi Flutter Pertama'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(randomWord, style: const TextStyle(fontSize: 40)),
+            IconButton(
+              icon: Icon(Icons.favorite_border),
+              onPressed: _saveWord,
+            )
+          ],
         ),
-        body: Center(
-          child:
-              Text(randomWord.asString, style: const TextStyle(fontSize: 40)),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.play_arrow),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _changeWord,
+        child: const Icon(Icons.play_arrow),
       ),
     );
   }
